4e9b04b82f5f1506c8b76ada13ccbcbd56c1c564
ptagliolato <ptagliolato@users.noreply.github.com>
2019-04-05 11:21:12 +0200

    ... fixing urls ...

--- geosk/tellme/urls.py
+++ geosk/tellme/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     # overwrite API EdiProxy. The new version will override the mdtools.api one
     # in order to intercept TELLme keywords properly
     #url(r'^ediproxy/(?P<layername>[^/]*)/importmd$', 'geosk.mdtools.api.ediproxy_importmd', name='ediproxy_importmd')l
-    url(r'^whoami$', 'geosk.mdtools.views.whoami', name='whoami',
+    url(r'^whoami$', 'geosk.mdtools.views.whoami', name='whoami'),
 ]
