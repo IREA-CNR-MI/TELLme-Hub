6a78533334ea23bca91e038a0e82ac7e27691e75
ptagliolato <ptagliolato@users.noreply.github.com>
2019-05-02 14:40:55 +0200

    Add configurabilty of EMAIL_* settings: read them from django.env or leave defaults

--- geosk/settings.py
+++ geosk/settings.py
@@ -487,6 +487,15 @@ if 'geonode.geoserver' in INSTALLED_APPS:
 # notification settings
 NOTIFICATION_ENABLED = True
 
+# email settings (overrides)
+if EMAIL_ENABLE:
+    EMAIL_HOST = os.getenv("EMAIL_HOST", EMAIL_HOST)
+    EMAIL_PORT = os.getenv("EMAIL_PORT", EMAIL_PORT)
+    EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", EMAIL_HOST_USER)
+    EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", EMAIL_HOST_PASSWORD)
+    EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", EMAIL_USE_TLS)
+    DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", DEFAULT_FROM_EMAIL)
+
 # notifications backends
 _EMAIL_BACKEND = "pinax.notifications.backends.email.EmailBackend"
 PINAX_NOTIFICATIONS_BACKENDS = [
