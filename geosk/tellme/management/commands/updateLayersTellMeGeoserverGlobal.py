c7a792351460b1fd8a4e895df79cd9192a1dd3ab
ptagliolato <ptagliolato@users.noreply.github.com>
2019-08-08 18:38:37 +0200

    Add command to update external layers of an already registered service (TELLmeGeoserverGlobal WMS, at the moment)

---
+++ geosk/tellme/management/commands/updateLayersTellMeGeoserverGlobal.py
@@ -0,0 +1,18 @@
+import logging
+#from django.conf import settings
+from django.core.management.base import BaseCommand
+from geonode.services.models import Service
+from geonode.services.views import _register_indexed_layers
+
+log = logging.getLogger(__name__)
+
+class Command(BaseCommand):
+    help = 'TELLmeGeoserverGlobal. Update layers with new ones'
+
+    def handle(self, *args, **options):
+        try:
+            # service=Service.objects.all()[1] # in questo caso è  <Service: TELLmeGeoserverGlobal>
+            service = Service.objects.get(title="TELLmeGeoserverGlobal")
+            _register_indexed_layers(service, verbosity=True)
+        except Exception as e:
+            log.error(e.message)
