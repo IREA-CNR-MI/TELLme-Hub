69f2aeb8ae76c639b0f47592a013129b35c1c5ee
ptagliolato <ptagliolato@users.noreply.github.com>
2019-08-09 16:59:00 +0200

    Add synch sparql endpoint

--- geosk/tellme/tellmeGlossaryIntegration.py
+++ geosk/tellme/tellmeGlossaryIntegration.py
@@ -755,28 +755,28 @@ def synchGlossaryWithHierarchicalKeywords(g, force=True):
     #    If a slug2glos id of type keyword is not present within the glossary, remove it from Tellme branch and put it under
     #    the "_other" branch of HK
 
-# def synchSparqlEndpoint(endpoint="", user="", password=""):
-#     import requests
-#     import os
-#
-#     url = "{endpoint}/update".format(endpoint=os.getenv("TELLME_SPARQL_ENDPOINT", endpoint))
-#     user = os.getenv("SPARQL_ENDPOINT_USER", user)
-#     password = os.getenv("SPARQL_ENDPOINT_PASSWORD", password)
-#
-#     glossaryTTLurl = u"http://{dns}/static/tellme/static/tellme/TELLmeGlossary.ttl".format(dns=os.getenv("GEONODE_LB_HOST_IP"))
-#
-#     data = {'update': 'DELETE \nwhere{?s ?p ?o}\n\n#'}
-# 
-#     responseDelete = requests.post('endpoint', data=data,
-#                              auth=('user', 'password'))
-#
-#
-#     data = {'update': 'LOAD <{glossaryTTLurl}>'.format(glossaryTTLurl = glossaryTTLurl)}
-#
-#     responseLoad = requests.post('endpoint', data=data,
-#                              auth=('user', 'password'))
-#
-#     return responseDelete, responseLoad
+def synchSparqlEndpoint(endpoint="", user="", password=""):
+    import requests
+    import os
+
+    url = "{endpoint}/update".format(endpoint=os.getenv("TELLME_SPARQL_ENDPOINT", endpoint))
+    user = os.getenv("SPARQL_ENDPOINT_USER", user)
+    password = os.getenv("SPARQL_ENDPOINT_PASSWORD", password)
+
+    glossaryTTLurl = u"http://{dns}/static/tellme/static/tellme/TELLmeGlossary.ttl".format(dns=os.getenv("GEONODE_LB_HOST_IP"))
+
+    data = {'update': 'DELETE \nwhere{?s ?p ?o}\n\n#'}
+
+    responseDelete = requests.post('endpoint', data=data,
+                             auth=('user', 'password'))
+
+
+    data = {'update': 'LOAD <{glossaryTTLurl}>'.format(glossaryTTLurl = glossaryTTLurl)}
+
+    responseLoad = requests.post('endpoint', data=data,
+                             auth=('user', 'password'))
+
+    return responseDelete, responseLoad
 
 
 
