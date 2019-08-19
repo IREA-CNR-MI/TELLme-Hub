807274bb7175579547349318978ca1d78d87e3e0
ptagliolato <ptagliolato@users.noreply.github.com>
2019-08-19 16:27:24 +0200

    Delete non-tellme Hierarchical Keywords in synchronizeNewGlossaryEntries command

--- geosk/tellme/management/commands/synchronizeNewGlossaryEntries.py
+++ geosk/tellme/management/commands/synchronizeNewGlossaryEntries.py
@@ -5,15 +5,16 @@ from django.core.management.base import BaseCommand
 #from geosk.tellme.tellmeGlossaryIntegration import dumpTTLGlossaryToStaticDir
 #from geosk.tellme.tellmeGlossaryIntegration import synchGlossaryWithHierarchicalKeywords
 #from geosk.tellme.tellmeGlossaryIntegration import TellMeGlossary
-from geosk.tellme.tellmeGlossaryIntegration import synchNewKeywordsFromTELLmeGlossary
+from geosk.tellme.tellmeGlossaryIntegration import synchNewKeywordsFromTELLmeGlossary, delete_non_tellme_hierarchicalKeywords
 
 log = logging.getLogger(__name__)
 
 class Command(BaseCommand):
-    help = 'TELLme Glossary. Synch HierarchicalKeywords only with new terms in tellme glossary software'
+    help = 'TELLme Glossary. Synch HierarchicalKeywords only with new terms in tellme glossary software and delete all non-tellme hierarchical keywords'
 
     def handle(self, *args, **options):
         try:
+            delete_non_tellme_hierarchicalKeywords()
             synchNewKeywordsFromTELLmeGlossary()
 
         except Exception as e:
